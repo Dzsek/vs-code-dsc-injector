8b2a123ba60c4242426dec19f7da8add7f7dce31
Dzsek <dzsekeb@gmail.com>
2023-07-15 19:40:51 +0300

    added license

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2023, Dzsekeb
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. 
+*/
+
 import * as vscode from 'vscode';
 import * as net from 'net';
 import { InjectableScriptProvider } from './Providers/InjectableScriptProvider';
